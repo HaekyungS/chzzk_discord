96a5f43c6284983db4bb20b7d4b5179236f835f1
HaekyungSeong <haekyungs95@gmail.com>
2024-01-02 21:49:36 +0900

    주석 추가

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const client = new Discord.Client({
 client.on("ready", () => {
   console.log(`Woking ${client.user.tag}!`);
 
+  // 라이브 상태에 대한 변수
   let liveStatus = "";
 
+  // 반복적인 확인을 위해 setInterval
   setInterval(() => {
     // 채널아이디를 변수에 담기.
     const channelID = process.env.Channel_ID_natural;
@@ -26,6 +28,7 @@ client.on("ready", () => {
     // 채널불러오기.
     const channel = client.channels.cache.get(channelID);
 
+    // 라이브상태를 최근라이브상태변수에 대입
     let recentState = liveStatus;
 
     // fetch 로 치지직 api 에서 방송중여부 가져오기.
@@ -37,14 +40,20 @@ client.on("ready", () => {
     })
       .then((response) => response.json())
       .then((data) => {
+        // 최근 라이브 상태를 api로 재확인
         recentState = data.content.status;
+
+        // 최근 라이브상태와 라이브상태가 같지않은 경우
         if (recentState !== liveStatus) {
+          // 만약 그 상태가 open 이라면 방송알림
           if (data.content.status === "OPEN") {
-            channel.send("방송중~" + process.env.RyuChzzk);
+            channel.send("대쟝왔다!! \n" + process.env.RyuChzzk);
           } else {
+            // 뱅종하면 console로 확인
             console.log("지금 아가는 쉬는 중");
           }
 
+          // 바뀐 경우에 바뀐 값을 라이브상태에 대입
           liveStatus = recentState;
         }
       })
